b6f2feb432dd6fd152d5239b9f0b29789e4c207a
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-24 17:03:13 -0600

    built out invoice_item_repo and tests

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -1,12 +1,10 @@
 require_relative 'load_file'
-require_relative 'repo_helper'
 require_relative 'invoice_item'
 
 class InvoiceItemRepository 
   attr_reader :invoice_items, :sales_engine
 
   include LoadFile
-  include RepoHelper
 
   def initialize(sales_engine)
     @invoice_items = []
@@ -31,6 +29,73 @@ class InvoiceItemRepository
 
   def random
     invoice_items.sample
+  end
+  
+  def find_invoice(id)
+  end
+
+  def find_item(item_id)
+  end
+
+  def find_by_id(id)
+    invoice_items.find { |invoice_items| invoice_items.id == id }
+  end
+
+  def find_by_item_id(item_id)
+    invoice_items.find { |invoice_items| invoice_items.item_id == item_id }
+  end
+
+
+  def find_by_invoice_id(invoice_id)
+    invoice_items.find { |invoice_items| invoice_items.id == invoice_id }
+  end
+
+  def find_by_quantity(quantity)
+    invoice_items.find { |items| items.quantity == quantity }
+  end
+
+  def find_by_unit_price(price)
+    invoice_items.find { |items| items.unit_price == price }
+  end
+
+  def find_by_created_at(created_date)
+    invoice_items.find { |items| items.created_at == created_date }
+  end
+
+  def find_by_updated_at(updated_date)
+    invoice_items.find { |items| items.updated_at == updated_date }
+  end
+
+  def find_all_by(attribute, value)
+    invoice_items.find_all { |item| item.send(attribute) == value }
+  end
+
+  def find_all_by_id(id)
+    invoice_items.find_all { |invoice_items| invoice_items.id == id }
+  end
+
+  def find_all_by_item_id(item_id)
+    invoice_items.find_all { |invoice_items| invoice_items.item_id == item_id }
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    invoice_items.find_all { |invoice_items| invoice_items.invoice_id == invoice_id }
+  end
+
+  def find_all_by_quantity(quantity)
+    invoice_items.find_all { |items| items.quantity == quantity }
+  end
+
+  def find_all_by_unit_price(price)
+    invoice_items.find_all { |items| items.unit_price == price }
+  end
+
+  def find_all_by_created_at(created_date)
+    invoice_items.find_all { |items| items.created_at == created_date }
+  end
+
+  def find_all_by_updated_at(updated_date)
+    invoice_items.find_all { |items| items.updated_at == updated_date }
   end
 end
 
--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -1,6 +1,6 @@
 require_relative '../test/test_helper'
 require_relative '../lib/invoice_item_repository'
-# require 'bigdecimal/util'
+require 'bigdecimal/util'
 
 class InvoiceItemRepositoryTest < Minitest::Test
 
@@ -16,10 +16,6 @@ class InvoiceItemRepositoryTest < Minitest::Test
     assert_equal 1, invoice_item_repository.invoice_items.first.id
     assert_equal 539, invoice_item_repository.invoice_items.first.item_id
     assert_equal 1, invoice_item_repository.invoice_items[0].invoice_id
-
-    refute_equal "Kyle", invoice_item_repository.invoice_items.first.id
-    refute_equal 20, invoice_item_repository.invoice_items.first.item_id
-    refute_equal "Braslev", invoice_item_repository.invoice_items[0].invoice_id
   end
 
   def test_it_contains_all_invoice_items
@@ -38,24 +34,21 @@ class InvoiceItemRepositoryTest < Minitest::Test
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./fixtures/invoice_items.csv")
     result = invoice_item_repository.find_by_item_id(535)
-    # binding.pry
     assert_equal 535, result.item_id
   end
 
   def test_it_can_find_by_invoice_id
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./fixtures/invoice_items.csv")
-    result = invoice_item_repository.find_by(:invoice_id, 1)
-
+    result = invoice_item_repository.find_by_invoice_id(1)
     assert_equal 1, result.invoice_id
   end
 
   def test_it_can_find_by_quantity
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./fixtures/invoice_items.csv")
-    result = invoice_item_repository.find_all_by(:quantity, 9)
-
-    assert_equal 7, result.count
+    result = invoice_item_repository.find_by_quantity(9)
+    assert_equal 9, result.quantity
   end
 
   def test_it_can_find_by_unit_price
@@ -63,23 +56,20 @@ class InvoiceItemRepositoryTest < Minitest::Test
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./data/invoice_items.csv")
     result = invoice_item_repository.find_by_unit_price(BigDecimal.new(23324)/100)
-
     assert_equal "233.24", result.unit_price.to_digits
   end
 
   def test_it_can_find_by_created_at
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./data/invoice_items.csv")
-    result = invoice_item_repository.find_by(:created_at, "2012-03-27 14:54:09 UTC")
-
+    result = invoice_item_repository.find_by_created_at("2012-03-27 14:54:09 UTC")
     assert_equal "2012-03-27 14:54:09 UTC", result.created_at
   end
 
   def test_it_can_find_by_updated_at
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./data/invoice_items.csv")
-    result = invoice_item_repository.find_by(:updated_at, "2012-03-27 14:54:09 UTC")
-
+    result = invoice_item_repository.find_by_updated_at("2012-03-27 14:54:09 UTC")
     assert_equal "2012-03-27 14:54:09 UTC", result.updated_at
   end
 
@@ -87,7 +77,6 @@ class InvoiceItemRepositoryTest < Minitest::Test
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./data/invoice_items.csv")
     result = invoice_item_repository.find_all_by_id(4)
-
     assert_equal 1, result.count
   end
 
@@ -95,7 +84,6 @@ class InvoiceItemRepositoryTest < Minitest::Test
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./data/invoice_items.csv")
     result = invoice_item_repository.find_all_by_item_id(523)
-
     assert_equal 10, result.count
   end
 
@@ -103,7 +91,6 @@ class InvoiceItemRepositoryTest < Minitest::Test
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./fixtures/invoice_items.csv")
     result = invoice_item_repository.find_all_by_invoice_id(1)
-
     assert_equal 8, result.count
   end
 
@@ -111,7 +98,6 @@ class InvoiceItemRepositoryTest < Minitest::Test
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./fixtures/invoice_items.csv")
     result = invoice_item_repository.find_all_by(:quantity, 7)
-
     assert_equal 4, result.count
   end
 
@@ -120,7 +106,6 @@ class InvoiceItemRepositoryTest < Minitest::Test
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./data/invoice_items.csv")
     result = invoice_item_repository.find_all_by_unit_price(BigDecimal.new(72018)/100)
-
     assert_equal 33, result.count
   end
 
@@ -128,7 +113,6 @@ class InvoiceItemRepositoryTest < Minitest::Test
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./data/invoice_items.csv")
     result = invoice_item_repository.find_all_by_created_at("2012-03-27 14:54:10 UTC")
-
     assert_equal 97, result.count
   end
 
@@ -136,8 +120,36 @@ class InvoiceItemRepositoryTest < Minitest::Test
     invoice_item_repository = InvoiceItemRepository.new(nil)
     invoice_item_repository.load_data("./data/invoice_items.csv")
     result = invoice_item_repository.find_all_by_updated_at("2012-03-27 14:54:10 UTC")
-
     assert_equal 97, result.count
   end
 
+  def test_it_can_access_parent_with_items
+    skip
+    parent = Minitest::Mock.new
+    invoice_item_repository = InvoiceItemRepository.new(parent)
+    parent.expect(:find_item_by_id, [1, 2], [1])
+
+    assert_equal [1, 2], invoice_item_repository.find_item(1)
+    parent.verify
+  end
+
+  def test_it_can_access_parent_with_invoice
+    skip
+    parent = Minitest::Mock.new
+    invoice_item_repository = InvoiceItemRepository.new(parent)
+    parent.expect(:find_invoice_by_id, [1, 2], [1])
+
+    assert_equal [1, 2], invoice_item_repository.find_invoice(1)
+    parent.verify
+  end
+
+  def test_it_can_access_parent_with_item
+    skip
+    parent = Minitest::Mock.new
+    invoice_item_repository = InvoiceItemRepository.new(parent)
+    parent.expect(:find_item_by_id, [1, 2], [1])
+
+    assert_equal [1, 2], invoice_item_repository.find_item(1)
+    parent.verify
+  end
 end
