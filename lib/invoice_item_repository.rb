21f3b4cdbcb0e029e27abb94d7597d4e27ac4b4e
Dmitry Sky <dmitry.sky@icloud.com>
2015-04-23 22:42:08 -0600

    hard coded invoice_item_test_repo and invoice_item_repo

--- lib/invoice_item_repository.rb
+++ lib/invoice_item_repository.rb
@@ -23,7 +23,67 @@ class InvoiceItemRepository
     "#<#{self.class} #{@invoice_items.size} rows>"
   end
 
+  def all
+    invoice_items
+  end
+
   def random
     invoice_items.sample
+  end
+
+  def find_by_id(id)
+    invoice_items.find { |invoice_items| invoice_items.id == id }
+  end
+
+  def find_by_item_id(item_id)
+    invoice_items.find { |invoice_items| invoice_items.item_id == item_id }
+  end
+
+  def find_by_invoice_id(invoice_id)
+    invoice_items.find { |invoice_items| invoice_items.id == invoice_id }
+  end
+
+  def find_by_quantity(quantity)
+    invoice_items.find { |items| items.quantity == quantity }
+  end
+
+  def find_by_unit_price(price)
+    invoice_items.find { |items| items.unit_price == price }
+  end
+
+  def find_by_created_at(created_date)
+    invoice_items.find { |items| items.created_at == created_date }
+  end
+
+  def find_by_updated_at(updated_date)
+    invoice_items.find { |items| items.updated_at == updated_date }
+  end
+
+  def find_all_by_id(id)
+    invoice_items.find_all { |invoice_items| invoice_items.id == id }
+  end
+
+  def find_all_by_item_id(item_id)
+    invoice_items.find_all { |invoice_items| invoice_items.item_id == item_id }
+  end
+
+  def find_all_by_invoice_id(invoice_id)
+    invoice_items.find_all { |invoice_items| invoice_items.invoice_id == invoice_id }
+  end
+
+  def find_all_by_quantity(quantity)
+    invoice_items.find_all { |items| items.quantity == quantity }
+  end
+
+  def find_all_by_unit_price(price)
+    invoice_items.find_all { |items| items.unit_price == price }
+  end
+
+  def find_all_by_created_at(created_date)
+    invoice_items.find_all { |items| items.created_at == created_date }
+  end
+
+  def find_all_by_updated_at(updated_date)
+    invoice_items.find_all { |items| items.updated_at == updated_date }
   end
 end
--- test/invoice_item_repository_test.rb
+++ test/invoice_item_repository_test.rb
@@ -30,8 +30,121 @@ class InvoiceItemRepositoryTest < Minitest::Test
 
   def test_it_can_return_random_sample_of_invoice_item
     invoice_item_repository = InvoiceItemRepository.new(nil)
-    invoice_item_repository.load_data("../fixtures/invoice_item.csv")
+    invoice_item_repository.load_data("./fixtures/invoice_item.csv")
     assert invoice_item_repository.random
+  end
+
+    def test_it_can_find_by_id
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./fixtures/invoice_item.csv")
+    result = invoice_item_repository.find_by_id(4)
+    assert_equal 4, result.id
+  end
+
+    def test_it_can_find_by_item_id
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_by_item_id(539)
+
+    assert_equal 539, result.item_id
+  end
+
+  def test_it_can_find_by_invoice_id
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_by_invoice_id(1)
+
+    assert_equal 1, result.invoice_id
+  end
+
+  def test_it_can_find_by_quantity
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_by_quantity(9)
+
+    assert_equal 9, result.quantity
+  end
+
+  def test_it_can_find_by_unit_price
+    skip
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_by_unit_price(BigDecimal.new(23324)/100)
+
+    assert_equal "233.24", result.unit_price.to_digits
+  end
+
+  def test_it_can_find_by_created_at
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_by_created_at("2012-03-27 14:54:09 UTC")
+
+    assert_equal "2012-03-27 14:54:09 UTC", result.created_at
+  end
+
+  def test_it_can_find_by_updated_at
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_by_updated_at("2012-03-27 14:54:09 UTC")
+
+    assert_equal "2012-03-27 14:54:09 UTC", result.updated_at
+  end
+
+  def test_it_can_find_all_by_id
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_all_by_id(4)
+
+    assert_equal 1, result.count
+  end
+
+  def test_it_can_find_all_by_item_id
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_all_by_item_id(523)
+
+    assert_equal 10, result.count
+  end
+
+  def test_it_can_find_all_by_invoice_id
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_all_by_invoice_id(1)
+
+    assert_equal 8, result.count
+  end
+
+  def test_it_can_find_all_by_quantity
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_all_by_quantity(7)
+
+    assert_equal 2181, result.count
+  end
+
+  def test_it_can_find_all_by_unit_price
+    skip
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_all_by_unit_price(BigDecimal.new(72018)/100)
+
+    assert_equal 33, result.count
+  end
+
+  def test_it_can_find_all_by_created_at
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_all_by_created_at("2012-03-27 14:54:10 UTC")
+
+    assert_equal 97, result.count
+  end
+
+  def test_it_can_find_all_by_updated_at
+    invoice_item_repository = InvoiceItemRepository.new(nil)
+    invoice_item_repository.load_data("./data/invoice_items.csv")
+    result = invoice_item_repository.find_all_by_updated_at("2012-03-27 14:54:10 UTC")
+
+    assert_equal 97, result.count
   end
 
 end
